f62e1079a41bae71362fa5dbced8c497ecc89e3f
Sérgio Félix <sergio98@outlook.pt>
2024-06-19 04:12:58 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import networkx as nx
+from matplotlib import pyplot as plt
+
+options = {
+    "with_labels": True,
+    "node_size": 300,
+    "node_color": "lightblue",
+    "linewidths": 1.5,
+    "width": 1.5,
+    "edgecolors": "black",
+    "font_size": 10,
+    "font_color": "black",
+    "font_weight": "normal",
+    "font_family": "sans-serif",
+}
+
+layout_params = {
+    "k": 1,
+    "iterations": 50,
+}
 
 
 def calculate_weight(distance, time, consumption):
@@ -14,23 +33,6 @@ locals = {
     "M5": (41.279507, -8.253011),  # Igreja do Salvador de Aveleda
     "M6": (41.382566, -8.225727),  # Mosteiro de Santa Maria de Pombeiro
 }
-
-# roads_data = {
-#     ("A1", "M1", 5.5, 8, 8.5),
-#     ("A1", "M2", 5.1, 7, 6.8),
-#     ("A1", "M3", 12.5, 17, 7),
-#     ("A1", "M4", 7.7, 12, 6.7),
-#     ("M1", "M2", 10.9, 14, 4.8),
-#     ("M1", "M3", 11.2, 14, 9.2),
-#     ("M1", "M5", 8.3, 13, 5.5),
-#     ("M2", "M4", 5.6, 8, 5.2),
-#     ("M2", "M6", 7.5, 11, 5.3),
-#     ("M3", "M5", 5.4, 8, 7.1),
-#     ("M3", "M6", 6, 8, 5.9),
-#     ("M4", "M5", 7.5, 11, 5.3),
-#     ("M4", "M6", 11.2, 14, 7.5),
-#     ("M5", "M6", 3.6, 6, 6.3),
-# }
 
 roads_data = {
     ("A1", "M3", 5.5, 8, 8.5),
@@ -65,33 +67,6 @@ def create_network():
     return graph
 
 
-def shortest_path_from_accommodation_to_furthest_local(network, accommodation):
-    max_distance = 0
-    farthest_monument = None
-
-    for local in locals:
-        distance = nx.shortest_path_length(network, source=accommodation, target=local, weight='weight')
-        if distance > max_distance:
-            max_distance = distance
-            farthest_monument = local
-
-    shortest_path = nx.shortest_path(network, source=accommodation, target=farthest_monument, weight='weight')
-
-    return farthest_monument, round(max_distance, 2), shortest_path
-
-
-def shortest_path_between_every_two_locations(network):
-    shortest_paths = dict(nx.all_pairs_dijkstra_path(network, weight='weight'))
-    shortest_distances = dict(nx.all_pairs_dijkstra_path_length(network, weight='weight'))
-
-    for source in shortest_paths:
-        for target in shortest_paths[source]:
-            print(f"Shortest route from {source} to {target}:")
-            print(f"Path: {shortest_paths[source][target]}")
-            print(f"Total distance: {shortest_distances[source][target]} km")
-            print()
-
-
 def check_dirac_theorem(graph):
     n = graph.number_of_nodes()
     if n < 3:
@@ -118,9 +93,67 @@ def check_ore_theorem(graph):
     return True, "Graph satisfies Ore's condition and is Hamiltonian"
 
 
+def shortest_path_from_accommodation_to_furthest_local(network, accommodation):
+    max_distance = 0
+    farthest_monument = None
+
+    for local in locals:
+        distance = nx.shortest_path_length(network, source=accommodation, target=local, weight='weight')
+        if distance > max_distance:
+            max_distance = distance
+            farthest_monument = local
+
+    shortest_path = nx.shortest_path(network, source=accommodation, target=farthest_monument, weight='weight')
+
+    return farthest_monument, max_distance, shortest_path
+
+
+def shortest_path_between_every_two_locations(network):
+    shortest_paths = dict(nx.all_pairs_dijkstra_path(network, weight='weight'))
+    shortest_distances = dict(nx.all_pairs_dijkstra_path_length(network, weight='weight'))
+
+    for source in shortest_paths:
+        for target in shortest_paths[source]:
+            print(f"Shortest route from {source} to {target}:")
+            print(f"Path: {shortest_paths[source][target]}")
+            print(f"Total distance: {shortest_distances[source][target]:.2f}")
+            print()
+
+
 def tsp_minimum_cost_cycle(graph):
     tsp_cycle = nx.approximation.traveling_salesman_problem(graph, cycle=True, weight='weight')
-    return tsp_cycle
+    total_cost = 0
+    for u, v in zip(tsp_cycle, tsp_cycle[1:] + [tsp_cycle[0]]):
+        edge_data = graph.get_edge_data(u, v)
+        if edge_data is None:
+            continue
+        total_cost += edge_data['weight']
+    return tsp_cycle, total_cost
+
+
+def draw_networks(network):
+    filename = f'network.png'
+
+    pos = nx.spring_layout(network, **layout_params)
+
+    nx.draw(network, pos, **options)
+
+    plt.savefig(filename, dpi=300)
+    plt.close()
+
+
+def draw_tsp(network, path):
+    filename = f'tsp.png'
+
+    pos = nx.spring_layout(network, **layout_params)
+
+    nx.draw(network, pos, **options)
+
+    nx.draw_networkx_edges(network, pos, edgelist=[(path[i], path[i + 1]) for i in range(len(path) - 1)],
+                           edge_color='r', width=2.0)
+
+    plt.savefig(filename, dpi=300)
+    plt.close()
 
 
 def main():
@@ -143,7 +176,7 @@ def main():
     # Ex. 2. a)
     farthest_monument, max_distance, shortest_path = shortest_path_from_accommodation_to_furthest_local(network, "A1")
     print("Farthest monument:", farthest_monument)
-    print("Distance:", max_distance)
+    print(f"Distance: {max_distance:.2f}")
     print("Shortest path:", shortest_path)
     print("\n")
 
@@ -152,11 +185,14 @@ def main():
     print("\n")
 
     # Ex. 3. c)
-    path = tsp_minimum_cost_cycle(network)
+    path, cost = tsp_minimum_cost_cycle(network)
     print("Minimum cost Hamiltonian cycle starting at A1:")
     print(f"Path: {path}")
-    # print(f"Total cost: {cost}")
+    print(f"Total cost: {cost:.2f}")
     print("\n")
+
+    draw_networks(network)
+    draw_tsp(network, path)
 
 
 if __name__ == '__main__':
